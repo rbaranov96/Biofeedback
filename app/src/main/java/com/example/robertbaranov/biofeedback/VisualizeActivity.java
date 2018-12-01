a5059142d4cadcd7b9625492bfb4e1d56134ef7b
Evan <evan@evanreilly.com>
2018-12-01 11:30:07 -0500

    Fixed build issues, got heartrate measurement working, sends to textview every 10 seconds, made surfaceview tiny

--- app/src/main/java/com/example/robertbaranov/biofeedback/VisualizeActivity.java
+++ app/src/main/java/com/example/robertbaranov/biofeedback/VisualizeActivity.java
@@ -4,44 +4,90 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.hardware.Camera;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class VisualizeActivity extends AppCompatActivity {
+    private static final String TAG = "HeartRateMonitor";
+    private static final AtomicBoolean processing = new AtomicBoolean(false);
     private Button mQuestionButton;
-    private static Camera camera = null;
-    private static SurfaceHolder previewHolder = null;
-    private static PowerManager.WakeLock wakeLock = null;
-    private static long startTime = 0;
     private static SurfaceView preview = null;
-    private Context context;
+    private static SurfaceHolder previewHolder = null;
+    private static Camera camera = null;
+    private static View image = null;
+    private static TextView text = null;
+    private static String beatsPerMinuteValue="";
+    private static PowerManager.WakeLock wakeLock = null;
+
+
+    private static TextView mTxtVwStopWatch;
+    private static int averageIndex = 0;
+    private static final int averageArraySize = 4;
+    private static final int[] averageArray = new int[averageArraySize];
+    private static String strSavedDoctorID="";
+    private static Context parentReference = null;
+
+    public static enum TYPE {
+        GREEN, RED
+    };
+
+    private static TYPE currentType = TYPE.GREEN;
+
+    public static TYPE getCurrent() {
+        return currentType;
+    }
+
+    private static int beatsIndex = 0;
+    private static final int beatsArraySize = 3;
+    private static final int[] beatsArray = new int[beatsArraySize];
+    private static double beats = 0;
+    private static long startTime = 0;
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualize);
-        context = getApplicationContext();
+
+
+
+
         preview = (SurfaceView) findViewById(R.id.preview);
         previewHolder = preview.getHolder();
+
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        image = findViewById(R.id.image);
+        text = (TextView) findViewById(R.id.text);
+
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "DoNotDimScreen");
+
 
         mQuestionButton = (Button)findViewById(R.id.question_button);
         mQuestionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(VisualizeActivity.this, QuestionaireActivity.class);
                 startActivity(intent);
             }
@@ -55,11 +101,17 @@ public class VisualizeActivity extends AppCompatActivity {
         //    series.appendData(new DataPoint(i,i),true,100);
         //}
     }
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -68,142 +120,162 @@ public class VisualizeActivity extends AppCompatActivity {
         startTime = System.currentTimeMillis();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onPause() {
         super.onPause();
+
         wakeLock.release();
+
         camera.setPreviewCallback(null);
         camera.stopPreview();
         camera.release();
+        //text.setText("---");
         camera = null;
     }
+
     private static Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public void onPreviewFrame(byte[] data, Camera cam) {
-            if (data == null)
-                throw new NullPointerException();
+            if (data == null) throw new NullPointerException();
             Camera.Size size = cam.getParameters().getPreviewSize();
-            if (size == null)
-                throw new NullPointerException();
-//            if (!processing.compareAndSet(false, true))
-//                return;
+            if (size == null) throw new NullPointerException();
+
+            if (!processing.compareAndSet(false, true)) return;
+
             int width = size.width;
             int height = size.height;
-            // Process image
-            //int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(),height,width);
-            //gx=imgAvg;
-            //text1.setText("The average pixel value is : "+String.valueOf(imgAvg));
-            //imgAvg of Pixel average value, Log
-            //			Log.i(TAG, "imgAvg=" + imgAvg);
-            //if (imgAvg == 0 || imgAvg == 255) {
-            //    processing.set(false);
-            //    return;
-            //}
 
-            //int averageArrayAvg = 0;
-            //int averageArrayCnt = 0;
-//            for (int i = 0; i < averageArray.length; i++) {
-//                if (averageArray[i] > 0) {
-//                    averageArrayAvg += averageArray[i];
-//                    averageArrayCnt++;
-//                }
-//            }
+            int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
+            // Log.i(TAG, "imgAvg="+imgAvg);
+            if (imgAvg == 0 || imgAvg == 255) {
+                processing.set(false);
+                return;
+            }
 
-            //int rollingAverage = (averageArrayCnt > 0)?(averageArrayAvg/averageArrayCnt):0;
-//            TYPE newType = currentType;
-//            if (imgAvg < rollingAverage) {
-//                newType = TYPE.RED;
-//                if (newType != currentType) {
-//                    beats++;
-//                    flag=0;
-//                    text2.setText("The number of pulses is   "+String.valueOf(beats));
-//                    //					Log.e(TAG, "BEAT!! beats=" + beats);
-//                }
-//            } else if (imgAvg > rollingAverage) {
-//                newType = TYPE.GREEN;
-//            }
-//
-//            if (averageIndex == averageArraySize)
-//                averageIndex = 0;
-//            averageArray[averageIndex] = imgAvg;
-//            averageIndex++;
-//
-//            // Transitioned from one state to another to the same
-//            if (newType != currentType) {
-//                currentType = newType;
-//                //				image.postInvalidate();
-//            }
-//Get System End Time（ms）
-//            long endTime = System.currentTimeMillis();
-//            double totalTimeInSecs = (endTime - startTime) / 1000d;
-//            if (totalTimeInSecs >= 2) {
-//                double bps = (beats / totalTimeInSecs);
-//                int dpm = (int) (bps * 60d);
-//                if (dpm < 30 || dpm > 180||imgAvg<200) {
-//                    //Get System start Time（ms）
-//                    startTime = System.currentTimeMillis();
-//                    //beats : Total heartbeat
-//                    beats = 0;
-//                    processing.set(false);
-//                    return;
-//                }
-            //				Log.e(TAG, "totalTimeInSecs=" + totalTimeInSecs + " beats="+ beats);
-//                if (beatsIndex == beatsArraySize)
-//                    beatsIndex = 0;
-//                beatsArray[beatsIndex] = dpm;
-//                beatsIndex++;
-//                int beatsArrayAvg = 0;
-//                int beatsArrayCnt = 0;
-//                for (int i = 0; i < beatsArray.length; i++) {
-//                    if (beatsArray[i] > 0) {
-//                        beatsArrayAvg += beatsArray[i];
-//                        beatsArrayCnt++;
-//                    }
-//                }
-//                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
-//                text.setText("Your heart rate is : "+String.valueOf(beatsAvg)+"  zhi : "+String.valueOf(beatsArray.length)
-//                        +"    "+String.valueOf(beatsIndex)+"    "+String.valueOf(beatsArrayAvg)+"    "+String.valueOf(beatsArrayCnt));
-////Get System time（ms）
-//                startTime = System.currentTimeMillis();
-//                beats = 0;
-//            }
-//            processing.set(false);
+            int averageArrayAvg = 0;
+            int averageArrayCnt = 0;
+            for (int i = 0; i < averageArray.length; i++) {
+                if (averageArray[i] > 0) {
+                    averageArrayAvg += averageArray[i];
+                    averageArrayCnt++;
+                }
+            }
+
+            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+            TYPE newType = currentType;
+            if (imgAvg < rollingAverage) {
+                newType = TYPE.RED;
+                if (newType != currentType) {
+                    beats++;
+                    // Log.d(TAG, "BEAT!! beats="+beats);
+                }
+            } else if (imgAvg > rollingAverage) {
+                newType = TYPE.GREEN;
+            }
+
+            if (averageIndex == averageArraySize) averageIndex = 0;
+            averageArray[averageIndex] = imgAvg;
+            averageIndex++;
+
+            // Transitioned from one state to another to the same
+            if (newType != currentType) {
+                currentType = newType;
+                //image.postInvalidate();
+            }
+
+            long endTime = System.currentTimeMillis();
+            double totalTimeInSecs = (endTime - startTime) / 1000d; //corresponds directly to totalTimeInSecs, change proportionally...
+            if (totalTimeInSecs >= 10) { //CHANGE MEASUREMENT INTERVAL HERE
+                double bps = (beats / totalTimeInSecs);
+                int dpm = (int) (bps * 60d);
+                if (dpm < 30 || dpm > 180) {
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                    processing.set(false);
+                    return;
+                }
+
+                // Log.d(TAG,
+                // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
+
+                if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                beatsArray[beatsIndex] = dpm;
+                beatsIndex++;
+
+                int beatsArrayAvg = 0;
+                int beatsArrayCnt = 0;
+                for (int i = 0; i < beatsArray.length; i++) {
+                    if (beatsArray[i] > 0) {
+                        beatsArrayAvg += beatsArray[i];
+                        beatsArrayCnt++;
+                    }
+                }
+                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+
+                text.setText(String.valueOf(beatsAvg));
+                beatsPerMinuteValue=String.valueOf(beatsAvg);
 
 
+                startTime = System.currentTimeMillis();
+                beats = 0;
+            }
+            processing.set(false);
         }
-
     };
+
+
+
     private static SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public void surfaceCreated(SurfaceHolder holder) {
             try {
                 camera.setPreviewDisplay(previewHolder);
                 camera.setPreviewCallback(previewCallback);
             } catch (Throwable t) {
-                //				Log.e("PreviewDemo-surfaceCallback","Exception in setPreviewDisplay()", t);
+                Log.e("PreviewDemo-surfcCalbck", "Exception in setPreviewDisplay()", t);
             }
         }
 
-        public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                                   int height) {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             Camera.Parameters parameters = camera.getParameters();
             parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
             Camera.Size size = getSmallestPreviewSize(width, height, parameters);
             if (size != null) {
                 parameters.setPreviewSize(size.width, size.height);
-                //				Log.d(TAG, "Using width=" + size.width + " height="	+ size.height);
+                Log.d(TAG, "Using width=" + size.width + " height=" + size.height);
+
             }
             camera.setParameters(parameters);
             camera.startPreview();
         }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
             // Ignore
         }
     };
-    private static Camera.Size getSmallestPreviewSize(int width, int height,
-                                                      Camera.Parameters parameters) {
+
+    private static Camera.Size getSmallestPreviewSize(int width, int height, Camera.Parameters parameters) {
         Camera.Size result = null;
+
         for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
             if (size.width <= width && size.height <= height) {
                 if (result == null) {
@@ -211,13 +283,39 @@ public class VisualizeActivity extends AppCompatActivity {
                 } else {
                     int resultArea = result.width * result.height;
                     int newArea = size.width * size.height;
-                    if (newArea < resultArea)
-                        result = size;
+
+                    if (newArea < resultArea) result = size;
                 }
             }
         }
+
         return result;
     }
+
+
+
+//    private static void prepareCountDownTimer(){
+//        mTxtVwStopWatch.setText("---");
+//        new CountDownTimer(10000, 1000) {
+//
+//            public void onTick(long millisUntilFinished) {
+//                mTxtVwStopWatch.setText("seconds remaining: " + (millisUntilFinished) / 1000);
+//            }
+//
+//            public void onFinish() {
+//                mTxtVwStopWatch.setText("done!");
+//            }
+//        }.start();
+//    }
+
+
+
+
+
+
+
+
+
 
 
 
